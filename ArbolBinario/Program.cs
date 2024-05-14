7cb2dab9dd8d537b1499d509f99a56d45f92dfaa
Usuario <Usuario@DEVELOPER02>
2024-05-14 13:40:06 -0600

    Add project files.

---
+++ ArbolBinario/Program.cs
@@ -0,0 +1,9 @@
+﻿using ArbolBinario;
+
+ArbolBinario2 arbolBinario = new ArbolBinario2();
+
+arbolBinario.PoblarArbol(arbolBinario.NodoRaiz);
+
+arbolBinario.RecorridoPreorden(arbolBinario.NodoRaiz);
+
+Console.ReadKey();
